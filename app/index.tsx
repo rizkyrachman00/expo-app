b5320bec25cdf941ba97ab58c084b8345b37e6d4
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-05-04 22:36:15 +0700

    feat: setup font family to entire app

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-bold">BlackBox Camp</Text>
+      <Text className="text-3xl text-accent font-rubik-medium">BlackBox Camp</Text>
     </View>
   );
 }
