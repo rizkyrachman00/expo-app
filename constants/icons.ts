eb2d6a47f9bb01f2a98a185a281fed331cca813d
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-05-04 23:51:10 +0700

    feat: create constants

---
+++ constants/icons.ts
@@ -0,0 +1,15 @@
+import arrow from "@/assets/icons/arrow.png";
+import home from "@/assets/icons/home.png";
+import person from "@/assets/icons/person.png";
+import save from "@/assets/icons/save.png";
+import search from "@/assets/icons/search.png";
+import star from "@/assets/icons/star.png";
+
+export const icons = {
+  home,
+  search,
+  person,
+  save,
+  star,
+  arrow,
+};
---
+++ constants/images.ts
@@ -0,0 +1,7 @@
+import highlight from "@/assets/images/highlight.png";
+import logo from "@/assets/images/icon.png";
+
+export const images = {
+  highlight,
+  logo
+};
