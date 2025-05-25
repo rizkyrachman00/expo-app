4f16ad25c2cd8e468b29935ea552df9536fc400a
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-05-25 13:29:26 +0700

    chore: update package, add package (expo-linear-gradient)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
@@ -17,8 +21,11 @@ module.exports = {
         primary: "#000000",
         secondary: "#ffffff",
         accent: "#FCA311",
-      }
+      },
     },
   },
   plugins: [],
-}
+  nativewind: {
+    styledComponents: ["LinearGradient"],
+  },
+};
