193f05eb7914c111a3c58758860add031d034144
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-05-25 13:35:38 +0700

    chore: up assets trainer images, setup images global data type, setup data trainer images

---
+++ constants/trainer-images.ts
@@ -0,0 +1,35 @@
+import yogaImage from "@/assets/images/trainers/yoga-image.webp";
+import dimasImage from "@/assets/images/trainers/dimas-image.webp";
+import wahyuImage from "@/assets/images/trainers/wahyu-image.webp";
+import kevinImage from "@/assets/images/trainers/kevin-image.webp";
+import dafaImage from "@/assets/images/trainers/dafa-image.webp";
+import ghifariImage from "@/assets/images/trainers/ghifari-image.webp";
+import rafiImage from "@/assets/images/trainers/rafi-image.webp";
+import endahImage from "@/assets/images/trainers/endah-image.webp";
+import fransischaImage from "@/assets/images/trainers/fransischa-image.webp";
+import nerdlyyyImage from "@/assets/images/trainers/nerdlyyy-image.webp";
+import nickoImage from "@/assets/images/trainers/nicko-image.webp";
+import riyanImage from "@/assets/images/trainers/riyan-image.webp";
+import rizkaImage from "@/assets/images/trainers/rizka-image.webp";
+import rositaImage from "@/assets/images/trainers/rosita-image.webp";
+import utchanImage from "@/assets/images/trainers/utchan-image.webp";
+import vieImage from "@/assets/images/trainers/vie-image.webp";
+
+export const trainerImages = {
+  yogaImage,
+  dimasImage,
+  wahyuImage,
+  kevinImage,
+  dafaImage,
+  ghifariImage,
+  rafiImage,
+  endahImage,
+  fransischaImage,
+  nerdlyyyImage,
+  nickoImage,
+  riyanImage,
+  rizkaImage,
+  rositaImage,
+  utchanImage,
+  vieImage,
+};
