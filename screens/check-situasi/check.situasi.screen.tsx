cb6d57dae93ca208edf7e86b6dcc98aad45f8e89
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-05-29 02:02:27 +0700

    refactor: kode label indicator status pengunjung dibuat menjadi komponen terpisah

--- screens/check-situasi/check.situasi.screen.tsx
+++ screens/check-situasi/check.situasi.screen.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GymLocations } from "@/constants/data/gym.locations";
+import { LabelStatusIndicator } from "@/components/check-situasi/label.status.indicator";
 
 const CheckSituasiScreen = () => {
   return (
@@ -24,40 +25,7 @@ const CheckSituasiScreen = () => {
       </View>
 
       {/* Label Situasi Pengunjung */}
-      <View className="flex flex-row justify-start gap-4 w-full bg-gray-900 rounded-lg p-4 mt-5">
-        <View className="flex flex-row gap-2 items-center">
-          <LinearGradient
-            colors={["#84cc16", "#4ade80"]}
-            start={{ x: 0, y: 0.5 }}
-            end={{ x: 1, y: 0.5 }}
-            className="h-4 w-4"
-            style={{ borderRadius: 10 }}
-          />
-          <Text className="text-sm text-white">Sepi</Text>
-        </View>
-
-        <View className="flex flex-row gap-2 items-center">
-          <LinearGradient
-            colors={["#38bdf8", "#e879f9"]}
-            start={{ x: 0, y: 0.5 }}
-            end={{ x: 1, y: 0.5 }}
-            className="h-4 w-4"
-            style={{ borderRadius: 10 }}
-          />
-          <Text className="text-sm text-white">Normal</Text>
-        </View>
-
-        <View className="flex flex-row gap-2 items-center">
-          <LinearGradient
-            colors={["#e879f9", "#dc2626"]}
-            start={{ x: 0, y: 0.5 }}
-            end={{ x: 1, y: 0.5 }}
-            className="h-4 w-4"
-            style={{ borderRadius: 10 }}
-          />
-          <Text className="text-sm text-white">Ramai</Text>
-        </View>
-      </View>
+      <LabelStatusIndicator />
 
       <VisitorCounter
         location={GymLocations.piyungan}
