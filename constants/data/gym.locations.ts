862f026c2108fbd4b81b75a0d3a6d03b68f750d0
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-05-29 01:49:55 +0700

    feat: update type, data (gym location)

--- constants/data/gym.locations.ts
+++ constants/data/gym.locations.ts
@@ -4,9 +4,11 @@ export const GymLocations = {
   piyungan: {
     branchIdentifier: "blackbox_1",
     name: "Piyungan",
+    capacity: 20,
   },
   jogja_kota: {
     branchIdentifier: "blackbox_2",
     name: "Jogja Kota",
+    capacity: 25,
   },
 } satisfies Record<string, Location>;
--- types/location.ts
+++ types/location.ts
@@ -3,4 +3,5 @@ export type BranchIdentifier = "blackbox_1" | "blackbox_2";
 export type Location = {
   branchIdentifier: BranchIdentifier;
   name: string;
+  capacity: number;
 };
